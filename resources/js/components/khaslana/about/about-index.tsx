631b8fa4f2b4b24777a8d0456f4174cc6e4e6420
Fajri Bagaskara Budi <fajribagaskara69@gmail.com>
2026-05-02 22:08:07 +0700

    chore: added about us (in progress)

--- resources/js/components/khaslana/about/about-index.tsx
+++ resources/js/components/khaslana/about/about-index.tsx
@@ -1,7 +1,52 @@
 export default function AboutIndex() {
     return (
-        <div className="flex flex-col items-center px-6 lg:px-[55px] pt-28 lg:justify-center">
-            ini tentang kami
+        <div>
+            <div className="what-section">
+                <div className="what-left">
+                    <h1>Apa itu <i>Khaslana?</i></h1>
+
+                    <p>Khaslana adalah platform digital kolaboratif yang didedikasikan sebagai wadah pemberdayaan bagi UMKM lokal Indonesia. Platform ini hadir untuk menjembatani produk-produk kreatif hasil karya anak bangsa—mulai dari kuliner, kerajinan tangan, hingga fashion—dengan pasar yang lebih luas melalui ekosistem yang modern dan inklusif.</p>
+                </div>
+
+                <div className="what-right">
+                    <img src="./../../../assets/tentang-kami/what-img.png" alt="" />
+                </div>
+            </div>
+
+            <div className="visi-misi-section">
+                <div className="visi-left">
+                    <img src="./../../../assets/tentang-kami/visi-misi.png" alt="" />
+                </div>
+
+                <div className="visi-right">
+                    <div className="visi-section">
+                        <span>VISI KAMI</span>
+
+                        <h2>Menjadi platform ekosistem UMKM terpercaya yang menghubungkan produk lokal Indonesia ke pasar yang lebih luas.</h2>
+
+                        <p>Kami percaya bahwa setiap warung, bengkel, dan pengrajin lokal berhak mendapatkan akses untuk menjangkau pasar tanpa kehilangan identitas mereka.</p>
+                    </div>
+
+                    <div className="misi-section">
+                        <span>MISI KAMI</span>
+
+                        <div className="misi">
+                            <img src="./../../../assets/tentang-kami/misi.svg" alt="" />
+                            <p>Memperluas akses UMKM ke pembeli, mitra, dan sumber daya yang sebelumnya sulit dijangkau. </p>
+                        </div>
+
+                        <div className="misi">
+                            <img src="./../../../assets/tentang-kami/misi.svg" alt="" />
+                            <p>Menyediakan platform yang memudahkan UMKM mengelola usaha, mulai dari pencatatan hingga pemasaran digital.</p>
+                        </div>
+
+                        <div className="misi">
+                            <img src="./../../../assets/tentang-kami/misi.svg" alt="" />
+                            <p>Mendampingi UMKM dalam adopsi teknologi secara bertahap sesuai kebutuhan dan kapasitas mereka.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }

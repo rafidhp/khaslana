e4b3522e25975529431ca20852fdd834c01a4cf4
Zaky Rizzan Zain <zakyngoding.exe@gmail.com>
2026-05-13 21:27:24 +0700

    chore : ngubah bentuk data dummy

--- resources/js/components/khaslana/catalog/catalog-index.tsx
+++ resources/js/components/khaslana/catalog/catalog-index.tsx
@@ -1,23 +1,10 @@
-import antingImg from "@/assets/images/catalog/anting-perak.png";
-import kopiImg from "@/assets/images/catalog/kopi-gayo.png";
-import sendalImg from "@/assets/images/catalog/sendal-garut.png";
-import vaseImg from "@/assets/images/catalog/vase-keramik.png";
 import { HeroSection } from '@/components/khaslana/catalog/hero-section';
 import { PaginationSection } from '@/components/khaslana/catalog/pagination-section';
 import { ProductCard } from '@/components/khaslana/catalog/product-card';
+// Import data dummy
+import { displayProducts } from '@/components/khaslana/catalog/product-data';
 
 export default function CatalogIndex() {
-    const productsData = [
-        { id: 1, name: "Vase Keramik Kasongan", price: "Rp 185.000", image: vaseImg, discount: "67%", rating: "4.9", location: "Kab. Bandung", sold: "205", slug: "vase-keramik-kasongan" },
-        { id: 2, name: "Sandalias Kulit Garut", price: "Rp 320.000", image: sendalImg, rating: "4.5", location: "Kab. Bandung", sold: "102", slug: "sendal-kulit-garut" },
-        { id: 3, name: "Kopi Arabica Gayo", price: "Rp 145.000", image: kopiImg, discount: "19%", rating: "4.5", location: "Kab. Bandung", sold: "103", slug: "kopi-arabica-gayo" },
-        { id: 4, name: "Anting Perak Kotagede", price: "Rp 450.000", image: antingImg, rating: "4.5", location: "Kab. Bandung", sold: "108", slug: "anting-perak-kotagede" }
-    ];
-
-    const displayProducts = Array.from({ length: 24 }, (_, i) => ({
-        ...productsData[i % productsData.length],
-        id: i + 1
-    }));
 
     return (
         <div className="flex flex-col w-full px-6 lg:px-[70px] mx-auto">

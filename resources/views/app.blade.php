2604a73e49029d62f7a506dbf00326f05d29ae2f
rafidhp <rafiislamipasha27@gmail.com>
2026-06-23 17:19:05 +0700

    fix: default apps logo

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -36,7 +36,6 @@
         {{-- <link rel="icon" href="/favicon.ico" sizes="any">
         <link rel="icon" href="/favicon.svg" type="image/svg+xml"> --}}
         <link rel="shortcut icon" href="{{ asset('assets/icons/khaslana-logo-green.png') }}" type="image/x-icon">
-        <link rel="apple-touch-icon" href="/apple-touch-icon.png">
 
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
